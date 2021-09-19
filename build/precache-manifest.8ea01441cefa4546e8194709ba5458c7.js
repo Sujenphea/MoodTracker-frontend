b8a4c065abeebaf070b512efeaf8b7e13c3825ae
sujen <sujenphea@gmail.com>
2021-09-19 14:08:18 +1200

    Add build folder

---
+++ build/precache-manifest.8ea01441cefa4546e8194709ba5458c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce6272d908869a46afbb527feed5618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9972ddd24326acd6c3b",
+    "url": "/static/css/main.95842f3a.chunk.css"
+  },
+  {
+    "revision": "9e0445b10ef4f9db3758",
+    "url": "/static/js/2.ce51377c.chunk.js"
+  },
+  {
+    "revision": "8f61e92c65c2039ceb1e8b578f8981cf",
+    "url": "/static/js/2.ce51377c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9972ddd24326acd6c3b",
+    "url": "/static/js/main.6e778670.chunk.js"
+  },
+  {
+    "revision": "4d96312d6d7ead5633b9",
+    "url": "/static/js/runtime-main.d921d773.js"
+  },
+  {
+    "revision": "923a122e8b91e53f7e6e498d7bc4dfe3",
+    "url": "/static/media/backgroundDark.923a122e.jpg"
+  },
+  {
+    "revision": "f0ff29780fd10a97a1fae11f56a456d3",
+    "url": "/static/media/backgroundLight.f0ff2978.jpg"
+  }
+]);
